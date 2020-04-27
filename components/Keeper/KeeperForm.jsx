057eac7c5359fe1e8fd0e5e7a69f4a08c2f62ae1
MaxDBam <62358630+MaxDBam@users.noreply.github.com>
2020-04-27 22:19:09 +0300

    Upd

--- components/Keeper/KeeperForm.jsx
+++ components/Keeper/KeeperForm.jsx
@@ -12,85 +12,93 @@ export default class KeeperForm extends React.Component {
     isTodos: false,
   };
 
-  onSaveText = () => {};
+  onSaveText = (noteText) => {
+    keeperService.save(noteText);
+    this.props.onUpdateNotes();
+  };
 
   onSaveImage = (noteImg) => {
     keeperService.save(noteImg);
     this.props.onUpdateNotes();
   };
 
-  onSaveVideo = () => {};
+  onSaveVideo = (noteVideo) => {
+    keeperService.save(noteVideo);
+    this.props.onUpdateNotes();
+  };
 
-  onSaveTodos = () => {};
+  onSaveTodos = (noteTodos) => {
+    keeperService.save(noteTodos);
+    this.props.onUpdateNotes();
+  };
 
   render() {
     const { isText, isImage, isVideo, isTodos } = this.state;
     return (
-      <div className='row mt-5 card-form'>
+      <div className='row  card-form'>
         <div className='col-8'>
-          {isText && <FormText />}
+          {isText && <FormText onSaveText={this.onSaveText} />}
           {isImage && <FormImage onSaveImage={this.onSaveImage} />}
-          {isVideo && <FormVideo />}
-          {isTodos && <FormTodos />}
+          {isVideo && <FormVideo onSaveVideo={this.onSaveVideo} />}
+          {isTodos && <FormTodos onSaveTodos={this.onSaveTodos}/>}
         </div>
-        <div className='col-4' style={{ marginTop: '21px' }}>
-          <button
-            className='btn btn-primary'
-            style={{ height: '40px', marginRight: '5px' }}
-            onClick={() =>
-              this.setState({
-                isText: true,
-                isImage: false,
-                isVideo: false,
-                isTodos: false,
-              })
-            }
-          >
-            <i className='fas fa-font'></i>
-          </button>
-          <button
-            className='btn btn-danger'
-            style={{ height: '40px', marginRight: '5px' }}
-            onClick={() =>
-              this.setState({
-                isText: false,
-                isImage: true,
-                isVideo: false,
-                isTodos: false,
-              })
-            }
-          >
-            <i className='fas fa-image'></i>
-          </button>
-          <button
-            className='btn btn-warning'
-            style={{ height: '40px', marginRight: '5px' }}
-            onClick={() =>
-              this.setState({
-                isText: false,
-                isImage: false,
-                isVideo: true,
-                isTodos: false,
-              })
-            }
-          >
-            <i className='fas fa-video'></i>
-          </button>
-          <button
-            className='btn btn-info'
-            style={{ height: '40px', marginRight: '5px' }}
-            onClick={() =>
-              this.setState({
-                isText: false,
-                isImage: false,
-                isVideo: false,
-                isTodos: true,
-              })
-            }
-          >
-            <i className='fas fa-list'></i>
-          </button>
-        </div>
+
+        <button
+          className='btn btn-primary'
+          style={{ height: '40px', marginRight: '5px' }}
+          onClick={() =>
+            this.setState({
+              isText: true,
+              isImage: false,
+              isVideo: false,
+              isTodos: false,
+            })
+          }
+        >
+          <i className='fas fa-font'></i>
+        </button>
+        <button
+          className='btn btn-danger'
+          style={{ height: '40px', marginRight: '5px' }}
+          onClick={() =>
+            this.setState({
+              isText: false,
+              isImage: true,
+              isVideo: false,
+              isTodos: false,
+            })
+          }
+        >
+          <i className='fas fa-image'></i>
+        </button>
+        <button
+          className='btn btn-warning'
+          style={{ height: '40px', marginRight: '5px' }}
+          onClick={() =>
+            this.setState({
+              isText: false,
+              isImage: false,
+              isVideo: true,
+              isTodos: false,
+            })
+          }
+        >
+          <i className='fas fa-video'></i>
+        </button>
+        <button
+          className='btn btn-info'
+          style={{ height: '40px', marginRight: '5px' }}
+          onClick={() =>
+            this.setState({
+              isText: false,
+              isImage: false,
+              isVideo: false,
+              isTodos: true,
+            })
+          }
+        >
+          <i className='fas fa-list'></i>
+        </button>
       </div>
     );
   }
